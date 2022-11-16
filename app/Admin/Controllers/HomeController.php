9dd6e95fb7a63f13cffd019d4ef4b4c47df8bac0
miladtut <miladtut2@gmail.com>
2022-11-16 03:02:49 +0200

    first

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -8,6 +8,7 @@ use Encore\Admin\Controllers\Dashboard;
 use Encore\Admin\Layout\Column;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Layout\Row;
+use Encore\Admin\Widgets\Box;
 
 class HomeController extends Controller
 {
@@ -18,8 +19,23 @@ class HomeController extends Controller
             ->description('Description...')
             ->row(CustomDashboard::title())
             ->row(function (Row $row) {
-                $row->column(12, function (Column $column) {
-                    $column->append(CustomDashboard::environment());
+                $row->column(3, function (Column $column) {
+                    $labels = ["- 20", "20:30", "30:40", "40:50", "50:60", "+ 60"];
+                    $numbers = [12, 19, 3, 5, 2, 3];
+                    $data = [
+                        'labels'=>$labels,
+                        'numbers'=>$numbers
+                    ];
+                    $column->append(new Box('user age', view('admin.components.users-age-chart',['data'=>$data])));
+                });
+                $row->column(3, function (Column $column) {
+                    $labels = ["male", "female"];
+                    $numbers = [12, 19];
+                    $data = [
+                        'labels'=>$labels,
+                        'numbers'=>$numbers
+                    ];
+                    $column->append(new Box('user gender', view('admin.components.users-gender-chart',['data'=>$data])));
                 });
             });
     }
