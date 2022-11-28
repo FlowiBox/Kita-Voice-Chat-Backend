7bae9501a6493fae3b408eeacd2a433bae433494
miladtut <miladtut2@gmail.com>
2022-11-28 15:12:02 +0200

    any

--- app/Admin/Controllers/RoomController.php
+++ app/Admin/Controllers/RoomController.php
@@ -87,7 +87,7 @@ class RoomController extends Controller
         $grid->column('uid',trans ('room owner'))->display (function ($uid){
             $user = User::query ()->find ($uid);
             if($user){
-                return "<a href='users/$uid'>$user->name</a>";
+                return "<a href='users/$uid'>$user->name ? : no name</a>";
             }
             return "N/A";
         });
