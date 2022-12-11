67e338c2302eeceb517f44251604b79084b4a4f1
miladtut <miladtut2@gmail.com>
2022-12-11 14:46:54 +0200

    pass status

--- app/Http/Resources/Api/V1/RoomResource.php
+++ app/Http/Resources/Api/V1/RoomResource.php
@@ -29,6 +29,7 @@ class RoomResource extends JsonResource
             'is_hot'=>$this->hot,
             'is_popular'=>$this->is_popular,
             'room_status'=>$this->room_status,
+            'password_status'=>$this->room_pass ?true:false,
             'room_intro'=>$this->room_intro,
             'is_recommended'=>$this->is_recommended,
             'lang'=>$this->lang,
