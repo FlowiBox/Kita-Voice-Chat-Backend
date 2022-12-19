d4424a62ffe784c037957bc05d4001d88a3a1c21
miladtut <miladtut2@gmail.com>
2022-12-19 22:27:59 +0200

    rooms desk

--- app/Http/Controllers/Api/V1/RoomController.php
+++ app/Http/Controllers/Api/V1/RoomController.php
@@ -61,12 +61,13 @@ class RoomController extends Controller
     {
         $request['show']=true;
         $request['numid']=rand (111111,999999);
+        $request['room_name']=rand (111111,999999);
         try {
             $room = Room::query ()->where('uid',$request->user ()->id)->first ();
             if ($room){
                 $room->room_status = 1;
                 $room->save ();
-                return Common::apiResponse (true,'you are already have a room',new RoomResource($room),200);
+                return Common::apiResponse (true,'you are already have a room',new RoomResource($room),201);
             }
            $room = $this->repo->create (array_merge($request->all (),['uid'=>$request->user ()->id]));
             if ($request->hasFile ('room_cover')){
