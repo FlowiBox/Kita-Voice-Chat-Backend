157f95a27e08686bb15aa70eb1dcc683714989ee
shenouda <shenopro@gmail.com>
2023-02-06 14:00:32 +0200

    any

--- app/Http/Controllers/Api/V1/RoomController.php
+++ app/Http/Controllers/Api/V1/RoomController.php
@@ -429,7 +429,7 @@ class RoomController extends Controller
 
         foreach ($room_info as $k => &$v){
             if (!$v){
-                if (in_array ($k,['is_afk','gap','exp','sort','num','audio_sort','audio_num','strto_time'])){
+                if (in_array ($k,['is_afk','gap','exp','sort','num','audio_sort','audio_num','strto_time','is_pk'])){
                     $v = 0;
                 }else{
                     $v = '';
