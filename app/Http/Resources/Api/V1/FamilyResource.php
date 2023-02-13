602a1877605fc1bbdd4ed596df0085f6a4074854
milad <miladtut2@gmail.com>
2023-02-13 17:21:20 +0200

    any

--- app/Http/Resources/Api/V1/FamilyResource.php
+++ app/Http/Resources/Api/V1/FamilyResource.php
@@ -26,6 +26,8 @@ class FamilyResource extends JsonResource
             $owner = new \stdClass();
         }
 
+        $me = new UserResource($request->user ());
+
         $mems = FamilyUser::query ()->where ('family_id',@$this->id)->where ('status',1)->pluck ('user_id');
         return [
             'id'=>@$this->id,
@@ -37,6 +39,7 @@ class FamilyResource extends JsonResource
             'max_num_of_admins'=>@$this->num_admins?:0,
             'rank'=>@$this->rank?:0,
             'owner'=>$owner,
+            'me'=>$me,
             'am_i_member'=>FamilyUser::query ()->where ('user_id',$request->user ()->id)->where ('family_id',$this->id)->where ('status',1)->exists (),
             'am_i_owner'=>(@$this->user_id == $request->user ()->id) ?true:false,
             'am_i_admin'=>$request->user ()->is_family_admin ?true:false,
