e9f230bf518c39949dea91bb05f060f6117e28cc
milad <miladtut2@gmail.com>
2023-05-04 20:02:33 +0300

    any

--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -8,6 +8,7 @@ use App\Admin\Actions\DeleteUserVipAction;
 use App\Admin\Actions\EditPackExpireAction;
 use App\Admin\Actions\KickOfAgencyAction;
 use App\Admin\Actions\KickOfFamilyAction;
+use App\Admin\Forms\ProfileForm;
 use App\Helpers\Common;
 use App\Models\Agency;
 use App\Models\Charge;
@@ -27,6 +28,7 @@ use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Show;
 use Encore\Admin\Widgets\InfoBox;
+use Encore\Admin\Widgets\Tab;
 use Encore\Admin\Widgets\Table;
 use Illuminate\Support\Facades\App;
 
@@ -57,6 +59,22 @@ class UserController extends MainController
     public function __construct ()
     {
         $this->title ='Users';
+    }
+
+    public function index0 ( Content $content )
+    {
+        if (!Admin::user()->can('*')){
+            Permission::check('browse-users');
+        }
+
+        $forms = [
+            'one'=>ProfileForm::class,
+            'tow'=>ProfileForm::class,
+        ];
+
+        return $content
+            ->title(__($this->title))
+            ->body(Tab::forms($forms));
     }
 
     public function index ( Content $content )
@@ -247,8 +265,9 @@ class UserController extends MainController
 
         $form->display('id', __('id'));
         $form->text('uuid', __('uuid'));
-        $form->switch ('is_gold_id',__('Gold id'))->states (Common::getSwitchStates ());
+//        $form->switch ('is_gold_id',__('Gold id'))->states (Common::getSwitchStates ());
         $form->text('name', __('Name'));
+        $form->image ('profile.avatar',__ ('image'));
         $form->select ('country_id',trans ('country'))->options (function (){
             $ops = [0=>'no country'];
             $countries = Country::all ();
@@ -265,6 +284,8 @@ class UserController extends MainController
             }
             return $ops;
         });
+//        $form->date('profile.birthday',__ ('birthday'));
+        $form->select ('profile.gender',__ ('gender'))->options ([0=>__ ('female'),1=>__ ('male')]);
         $form->number ('di',__('coins'));
         $form->number ('gold',__('silver coins'));
         $form->email('email', __('Email'))->attribute ('onfocus',"this.removeAttribute('readonly');")->attribute ('readonly');
