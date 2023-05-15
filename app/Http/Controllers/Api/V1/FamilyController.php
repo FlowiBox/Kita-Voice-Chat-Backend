7ba985377e7957d28b323f2587c79dda44c1bbc4
rstardeveloper <rstardevelopder@gmail.com>
2023-05-15 03:50:38 -0700

    cut 15000 whene create family

--- app/Http/Controllers/Api/V1/FamilyController.php
+++ app/Http/Controllers/Api/V1/FamilyController.php
@@ -110,6 +110,9 @@ class FamilyController extends Controller
     public function store(Request $request)
     {
         $user = $request->user ();
+        if($user->coins < 15000){
+            return Common::apiResponse(0,'Insufficient balance, please go to recharge!',null,407);
+        }
         $ex = Family::query ()->where ('user_id',$user->id)->exists ();
         if ($ex) return Common::apiResponse (0,'already have family',null,405);
         $data = [
@@ -145,6 +148,7 @@ class FamilyController extends Controller
             $family_user->status = 1;
             $family_user->save ();
             $user->family_id = $family->id;
+            $user->coins = ($user->coins - 15000);
             $user->save();
             DB::commit ();
         }catch (\Exception $exception){
