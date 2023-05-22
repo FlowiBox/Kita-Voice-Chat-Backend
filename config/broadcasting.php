a7b7efc6e780c99550c7ac3e228abc2f415321ac
rstardeveloper <rstardevelopder@gmail.com>
2023-05-22 05:32:39 -0700

    update config

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,8 +37,8 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER', 'mt1'),
-                'encrypted' => true,
-                'useTLS' => true,
+                'encrypted' => false,
+                'useTLS' => false,
                 'host' => env('PUSHER_HOST', '127.0.0.1'),
                 'port' => env('PUSHER_PORT', '6001'),
                 'scheme' => env('PUSHER_SCHEME', 'http'),
