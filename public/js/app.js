46ecacd2d840a4a903359fa4718b3ed5c954a866
rstardeveloper <rstardevelopder@gmail.com>
2023-05-22 06:11:00 -0700

    update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -2168,7 +2168,7 @@ window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   key: "9bfa0b56e375267a8f59",
   cluster: "mt1",
   forceTLS: false,
-  wsHost: 'dragon-chat-app.com',
+  wsHost: '198.177.124.229',
   wsPort: 6001,
   disableStats: true,
   enabledTransports: ['ws'],
