84595472b3e9e3ab09bc4d3cb0d4f6bd80358349
rstardeveloper <rstardevelopder@gmail.com>
2023-05-25 04:48:18 -0700

    update resourse group chat

--- app/Http/Resources/Api/V1/GroupChatResource.php
+++ app/Http/Resources/Api/V1/GroupChatResource.php
@@ -25,15 +25,7 @@ class GroupChatResource extends JsonResource
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
-    {
-        $pass_status = false;
-        $now_room = Room::query ()->where ('uid',$this->user->now_room_uid)->first ();
-        if ($now_room){
-            if($now_room->room_pass){
-                $pass_status = true;
-            }
-        }
-        
+    {  
         $data = [
             'id'=>@$this->user->id,
             'uuid'=>@$this->user->uuid,
@@ -45,12 +37,6 @@ class GroupChatResource extends JsonResource
             ],
             'frame'=>Common::getUserDress($this->user->id,$this->user->dress_1,4,'img2')?:Common::getUserDress($this->user->id,$this->user->dress_1,4,'img1'),
             'frame_id'=>@$this->user->dress_1,
-            'now_room'=>[
-                'is_in_room'=>@$this->user->now_room_uid != 0,
-                'uid'=>@$this->user->now_room_uid,
-                'is_mine'=>@$this->user->id == $this->user->now_room_uid,
-                'password_status'=>$pass_status
-            ],
             'vip'=> [
                 'level' => @$this->user->UserVip->level,
             ],
@@ -58,10 +44,9 @@ class GroupChatResource extends JsonResource
                 'receiver_img' => @$this->user->getImageReceiverOrSender('receiver_id',1)->img,
                 'sender_img' => @$this->user->getImageReceiverOrSender('sender_id',2)->img, 
             ],
-            'online_time'=>@$this->user->online_time?date("Y-m-d H:i:s", @$this->user->online_time):'',
             'has_color_name'=>Common::hasInPack (@$this->user->id,18),
-            'is_follow'=>@(bool)Common::IsFollow (@$request->user->id,@$this->user->id),
-            'group_message' => $this->text
+            'group_message' => $this->text,
+            'created_at' => $this->created_at,
         ];
 
         return $data;
