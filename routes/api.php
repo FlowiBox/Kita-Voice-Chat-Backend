17487fe4b1d2a7d4f622c03a458ece28d3983e05
rstardeveloper <rstardevelopder@gmail.com>
2023-05-30 02:03:13 -0700

    fix api

--- routes/api.php
+++ routes/api.php
@@ -328,7 +328,7 @@ $connection = config( 'broadcasting.connections.pusher' );
     if(count($channels->channels) > 0)
     {
         if(!empty($request->room_id)){
-            $channel_name = 'presence-room-'+$request->room_id;
+            $channel_name = 'presence-room-'.$request->room_id;
             if(in_array($channel_name, $channels->channels)){
                 $info = $pusher->get_channel_info($channel_name, ['info' => 'user_count']);
                 $user_count = $info->user_count;
