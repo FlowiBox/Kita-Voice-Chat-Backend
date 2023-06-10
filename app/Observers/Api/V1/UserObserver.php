fd427dfca014d578d38f8509c927631c6c707544
milad <miladtut2@gmail.com>
2023-06-10 04:27:13 +0300

    logs

--- app/Observers/Api/V1/UserObserver.php
+++ app/Observers/Api/V1/UserObserver.php
@@ -112,7 +112,7 @@ class UserObserver
      * @param User $user
      * @return void
      */
-    public function updating (User $user){
+    public function updating000 (User $user){
         unset($user->is_follow);
         if ($user->agency_id){
             if ($user->is_host == 0){
@@ -255,115 +255,238 @@ class UserObserver
 
     }
 
-    public function saving(User $user){
-        if ($user->agency_id){
-            if ($user->is_host == 0){
-                $user->coins = 0;
-                GiftLog::query ()
-                    ->where ('receiver_id',$user->id)
-                    ->whereYear ('created_at',Carbon::now ()->year)
-                    ->whereMonth ('created_at',Carbon::now ()->month)
-                    ->delete ();
+    public function updating(User $user){
+
+        try {
+            if ($user->agency_id){
+                if ($user->is_host == 0){
+                    $user->coins = 0;
+                    GiftLog::query ()
+                        ->where ('receiver_id',$user->id)
+                        ->whereYear ('created_at',Carbon::now ()->year)
+                        ->whereMonth ('created_at',Carbon::now ()->month)
+                        ->delete ();
+                }
+                $user->is_host = 1;
             }
-            $user->is_host = 1;
-        }
-        if ($user->agency_id == 0 || $user->agency_id == null){
+            if ($user->agency_id == 0 || $user->agency_id == null){
+                if ($user->is_host == 1){
+                    $user->coins = 0;
+                }
+                $user->is_host = 0;
+            }
+            if ($user->status == 0){
+                $user->tokens()->delete ();
+            }
+
+            if (!$user->uuid){
+                $user->uuid = (string)rand (1000000,9999999);
+            }
+
+            $month_received = GiftLog::query ()
+                ->where ('receiver_id',$user->id)
+                ->whereYear ('created_at',Carbon::now ()->year)
+                ->whereMonth ('created_at',Carbon::now ()->month)
+                ->sum ('receiver_obtain');
+
+            if($month_received < 1){
+                $user->coins = 0;
+            }
+
             if ($user->is_host == 1){
+                $target = Target::query ()->where ('diamonds','<=',$month_received)->orderBy ('diamonds','desc')->first ();
+                if ($target){
+                    $hours = 0;
+                    $days = 0;
+                    $times = LiveTime::query ()->where ('uid',$user->id)
+                        ->whereYear ('created_at','=',Carbon::now ()->year)
+                        ->whereMonth ('created_at','=',Carbon::now ()->month)
+                        ->selectRaw('uid, count(hours) as hnum, count(days) as dnum')
+                        ->groupBy ('uid')
+                        ->first ()
+                    ;
+                    if ($times){
+                        $hours = $times->hnum;
+                        $days = $times->dnum;
+                    }
+                    $per = 0.50;
+                    if ($target->hours <= $hours){
+                        $per += 0.20;
+                    }
+                    if ($target->days <= $days){
+                        $per += 0.30;
+                    }
+                    $t = $target->usd * $per;
+                    $ap = $target->agency_share/100;
+                    $user->target_usd = $t;
+                    UserTarget::query ()->updateOrCreate (
+                        [
+                            'user_id'=>$user->id,
+                            'add_month'=>Carbon::now ()->month,
+                            'add_year'=>Carbon::now ()->year
+                        ],
+                        [
+                            'user_id'=>$user->id,
+                            'add_month'=>Carbon::now ()->month,
+                            'add_year'=>Carbon::now ()->year,
+                            'agency_id'=>$user->agency_id,
+                            'family_id'=>$user->family_id,
+                            'target_id'=>$target->id,
+                            'target_diamonds'=>$target->diamonds,
+                            'target_usd'=>$target->usd,
+                            'target_hours'=>$target->hours,
+                            'target_days'=>$target->days,
+                            'target_agency_share'=>$target->agency_share,
+                            'user_diamonds'=>$month_received,
+                            'user_hours'=>$hours,
+                            'user_days'=>$days,
+                            'user_obtain'=>$t,
+                            'agency_obtain'=>$t * $ap
+                        ]
+                    );
+
+                    UserSallary::query ()->updateOrCreate (
+                        [
+                            'user_id'=>$user->id,
+                            'month'=>Carbon::now ()->month,
+                            'year'=>Carbon::now ()->year
+                        ],
+                        [
+                            'user_id'=>$user->id,
+                            'month'=>Carbon::now ()->month,
+                            'year'=>Carbon::now ()->year,
+                            'sallary'=>$t,
+                            'agency_sallary'=>$t * $ap,
+                            'user_agency_id'=>$user->agency_id,
+                            'hours'=>"$hours / $target->hours",
+                            'days'=>"$days / $target->days"
+                        ]
+                    );
+
+
+                }
+            }
+        }catch (\Illuminate\Database\QueryException $e){
+            throw $e;
+        }
+
+    }
+
+    public function saving(User $user){
+
+        try {
+            if ($user->agency_id){
+                if ($user->is_host == 0){
+                    $user->coins = 0;
+                    GiftLog::query ()
+                        ->where ('receiver_id',$user->id)
+                        ->whereYear ('created_at',Carbon::now ()->year)
+                        ->whereMonth ('created_at',Carbon::now ()->month)
+                        ->delete ();
+                }
+                $user->is_host = 1;
+            }
+            if ($user->agency_id == 0 || $user->agency_id == null){
+                if ($user->is_host == 1){
+                    $user->coins = 0;
+                }
+                $user->is_host = 0;
+            }
+            if ($user->status == 0){
+                $user->tokens()->delete ();
+            }
+
+            if (!$user->uuid){
+                $user->uuid = (string)rand (1000000,9999999);
+            }
+
+            $month_received = GiftLog::query ()
+                ->where ('receiver_id',$user->id)
+                ->whereYear ('created_at',Carbon::now ()->year)
+                ->whereMonth ('created_at',Carbon::now ()->month)
+                ->sum ('receiver_obtain');
+
+            if($month_received < 1){
                 $user->coins = 0;
             }
-            $user->is_host = 0;
-        }
-        if ($user->status == 0){
-            $user->tokens()->delete ();
-        }
 
-        if (!$user->uuid){
-            $user->uuid = (string)rand (1000000,9999999);
-        }
+            if ($user->is_host == 1){
+                $target = Target::query ()->where ('diamonds','<=',$month_received)->orderBy ('diamonds','desc')->first ();
+                if ($target){
+                    $hours = 0;
+                    $days = 0;
+                    $times = LiveTime::query ()->where ('uid',$user->id)
+                        ->whereYear ('created_at','=',Carbon::now ()->year)
+                        ->whereMonth ('created_at','=',Carbon::now ()->month)
+                        ->selectRaw('uid, count(hours) as hnum, count(days) as dnum')
+                        ->groupBy ('uid')
+                        ->first ()
+                    ;
+                    if ($times){
+                        $hours = $times->hnum;
+                        $days = $times->dnum;
+                    }
+                    $per = 0.50;
+                    if ($target->hours <= $hours){
+                        $per += 0.20;
+                    }
+                    if ($target->days <= $days){
+                        $per += 0.30;
+                    }
+                    $t = $target->usd * $per;
+                    $ap = $target->agency_share/100;
+                    $user->target_usd = $t;
+                    UserTarget::query ()->updateOrCreate (
+                        [
+                            'user_id'=>$user->id,
+                            'add_month'=>Carbon::now ()->month,
+                            'add_year'=>Carbon::now ()->year
+                        ],
+                        [
+                            'user_id'=>$user->id,
+                            'add_month'=>Carbon::now ()->month,
+                            'add_year'=>Carbon::now ()->year,
+                            'agency_id'=>$user->agency_id,
+                            'family_id'=>$user->family_id,
+                            'target_id'=>$target->id,
+                            'target_diamonds'=>$target->diamonds,
+                            'target_usd'=>$target->usd,
+                            'target_hours'=>$target->hours,
+                            'target_days'=>$target->days,
+                            'target_agency_share'=>$target->agency_share,
+                            'user_diamonds'=>$month_received,
+                            'user_hours'=>$hours,
+                            'user_days'=>$days,
+                            'user_obtain'=>$t,
+                            'agency_obtain'=>$t * $ap
+                        ]
+                    );
 
-        $month_received = GiftLog::query ()
-            ->where ('receiver_id',$user->id)
-            ->whereYear ('created_at',Carbon::now ()->year)
-            ->whereMonth ('created_at',Carbon::now ()->month)
-            ->sum ('receiver_obtain');
+                    UserSallary::query ()->updateOrCreate (
+                        [
+                            'user_id'=>$user->id,
+                            'month'=>Carbon::now ()->month,
+                            'year'=>Carbon::now ()->year
+                        ],
+                        [
+                            'user_id'=>$user->id,
+                            'month'=>Carbon::now ()->month,
+                            'year'=>Carbon::now ()->year,
+                            'sallary'=>$t,
+                            'agency_sallary'=>$t * $ap,
+                            'user_agency_id'=>$user->agency_id,
+                            'hours'=>"$hours / $target->hours",
+                            'days'=>"$days / $target->days"
+                        ]
+                    );
 
-        if($month_received < 1){
-            $user->coins = 0;
-        }
 
-        if ($user->is_host == 1){
-            $target = Target::query ()->where ('diamonds','<=',$month_received)->orderBy ('diamonds','desc')->first ();
-            if ($target){
-                $hours = 0;
-                $days = 0;
-                $times = LiveTime::query ()->where ('uid',$user->id)
-                    ->whereYear ('created_at','=',Carbon::now ()->year)
-                    ->whereMonth ('created_at','=',Carbon::now ()->month)
-                    ->selectRaw('uid, count(hours) as hnum, count(days) as dnum')
-                    ->groupBy ('uid')
-                    ->first ()
-                ;
-                if ($times){
-                    $hours = $times->hnum;
-                    $days = $times->dnum;
                 }
-                $per = 0.50;
-                if ($target->hours <= $hours){
-                    $per += 0.20;
-                }
-                if ($target->days <= $days){
-                    $per += 0.30;
-                }
-                $t = $target->usd * $per;
-                $ap = $target->agency_share/100;
-                $user->target_usd = $t;
-                UserTarget::query ()->updateOrCreate (
-                    [
-                        'user_id'=>$user->id,
-                        'add_month'=>Carbon::now ()->month,
-                        'add_year'=>Carbon::now ()->year
-                    ],
-                    [
-                        'user_id'=>$user->id,
-                        'add_month'=>Carbon::now ()->month,
-                        'add_year'=>Carbon::now ()->year,
-                        'agency_id'=>$user->agency_id,
-                        'family_id'=>$user->family_id,
-                        'target_id'=>$target->id,
-                        'target_diamonds'=>$target->diamonds,
-                        'target_usd'=>$target->usd,
-                        'target_hours'=>$target->hours,
-                        'target_days'=>$target->days,
-                        'target_agency_share'=>$target->agency_share,
-                        'user_diamonds'=>$month_received,
-                        'user_hours'=>$hours,
-                        'user_days'=>$days,
-                        'user_obtain'=>$t,
-                        'agency_obtain'=>$t * $ap
-                    ]
-                );
-
-                UserSallary::query ()->updateOrCreate (
-                    [
-                        'user_id'=>$user->id,
-                        'month'=>Carbon::now ()->month,
-                        'year'=>Carbon::now ()->year
-                    ],
-                    [
-                        'user_id'=>$user->id,
-                        'month'=>Carbon::now ()->month,
-                        'year'=>Carbon::now ()->year,
-                        'sallary'=>$t,
-                        'agency_sallary'=>$t * $ap,
-                        'user_agency_id'=>$user->agency_id,
-                        'hours'=>"$hours / $target->hours",
-                        'days'=>"$days / $target->days"
-                    ]
-                );
-
-
             }
+        }catch (\Illuminate\Database\QueryException $e){
+            throw $e;
         }
+
     }
 
     /**
