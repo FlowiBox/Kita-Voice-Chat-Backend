957299a8f8c1b142dc4dea2bc54bfd130c730759
johnghattas <johnjon55555@gmail.com>
2023-06-26 15:52:06 +0200

    Update ExampleTest.php

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\Box;
+use App\Models\User;
+use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
@@ -13,6 +15,44 @@ class ExampleTest extends TestCase
      */
     public function test_example()
     {
-        $this->assertTrue(true);
+        $users = User::query()->orderByDesc('created_at')->limit(50)->get();
+
+        $boxIds = Box::query()->orderByDesc('created_at')->limit(10)->pluck('id');
+        $ifSend = true;
+        foreach ($users as $user){
+            $response = $this->actingAs($user)->post('/api/rooms/enter_room', ['owner_id' => 2160, 'room_pass' => 111111]);
+
+        }
+        // Make an authorized request and perform assertions
+//        $response = $this->actingAs($user)->post('/api/rooms/enter_room', ['owner_id' => 2160, 'room_pass' => 111111]);
+//        $response->assertStatus(200);
+    }
+
+
+    public function test_boxes()
+    {
+        $users = User::query()->orderByDesc('created_at')->limit(50)->get();
+
+        $boxIds = Box::query()->orderByDesc('created_at')->limit(10)->pluck('id');
+        $ifSend = true;
+        //        foreach ($users as $user){
+        //            $response = $this->actingAs($user)->post('/api/rooms/enter_room', ['owner_id' => 2160, 'room_pass' => 111111]);
+
+        if ($ifSend){
+            $user = $users->first();
+            $user->di = 99999999999999;
+            $user->save();
+
+            foreach ($boxIds as $boxId){
+                $response = $this->actingAs($user)->post('/api/box/send', ['box_id' => $boxId, 'room_uid' => 2160, 'user_num' => 7]);
+            }
+        }
+
+
+        //        }
+        // Make an authorized request and perform assertions
+        //        $response = $this->actingAs($user)->post('/api/rooms/enter_room', ['owner_id' => 2160, 'room_pass' => 111111]);
+        //        $response->assertStatus(200);
+        dd($response);
     }
 }
