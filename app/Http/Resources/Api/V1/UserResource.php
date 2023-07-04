3653cc17d35e06aeb43f00bd206c90badf2e3a2e
Ahmed Ramadan <56490896+AhmedRamadan30@users.noreply.github.com>
2023-07-04 11:00:20 +0300

    Update UserResource.php

--- app/Http/Resources/Api/V1/UserResource.php
+++ app/Http/Resources/Api/V1/UserResource.php
@@ -125,7 +125,7 @@ class UserResource extends JsonResource
             'number_of_friends'=>$this->numberOfFriends(),
             'profile_visitors'=>$this->profileVisits()->count(),
             'is_follow'=>@(bool)Common::IsFollow (@$request->user ()->id,$this->id),
-//            'is_friend'=>in_array ($this->id,$fArr),
+            'is_friend'=>in_array ($this->id,$fArr),
 //            'is_in_live'=>$this->is_in_live(),
             'is_first'=>@(bool)$this->is_points_first,
 //            'now_room'=>[
