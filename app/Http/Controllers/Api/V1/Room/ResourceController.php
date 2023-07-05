501ac6230a0cb667aec76bb56c1f1470e7e48f20
Ahmed Ramadan <56490896+AhmedRamadan30@users.noreply.github.com>
2023-07-05 18:22:21 +0300

    creating new ResourceController in folder room

---
+++ app/Http/Controllers/Api/V1/Room/ResourceController.php
@@ -0,0 +1,197 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Room;
+
+use App\Helpers\Common;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\EditRoomRequest;
+use App\Http\Resources\Api\V1\RoomResource;
+use App\Models\Agency;
+use App\Models\AgencySallary;
+use App\Models\Room;
+use App\Models\RoomCategory;
+use App\Models\RequestBackgroundImage;
+use App\Models\UserSallary;
+use App\Repositories\Room\RoomRepoInterface;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class ResourceController extends Controller
+{
+    protected $repo;
+
+    public function __construct (RoomRepoInterface $repo)
+    {
+        $this->repo = $repo;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(Request $request)
+    {
+        $result = $this->repo->all ($request);
+        $user = $request->user ();
+        foreach (Agency::all () as $agency){
+            AgencySallary::query ()->updateOrCreate (
+                [
+                    'agency_id'=>$agency->id,
+                    'month'=>Carbon::now ()->month,
+                    'year'=>Carbon::now ()->year
+                ],
+                [
+                    'agency_id'=>$agency->id,
+                    'month'=>Carbon::now ()->month,
+                    'year'=>Carbon::now ()->year,
+                    'sallary'=>UserSallary::query ()
+                        ->where ('month',Carbon::now ()->month)
+                        ->where ('year',Carbon::now ()->year)
+                        ->where ('user_agency_id',$agency->id)
+                        ->sum ('agency_sallary')
+                ]
+            );
+            $agency->salary;
+        }
+
+        return Common::apiResponse (true,'',RoomResource::collection ($result),200,Common::getPaginates ($result));
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        $request['show']=true;
+        $request['numid']=rand (111111,999999);
+        try {
+            $room = Room::query ()->where('uid',$request->user ()->id)->first ();
+            if ($room){
+                return Common::apiResponse (true,'you are already have a room',new RoomResource($room),200);
+            }
+            $room = $this->repo->create (array_merge($request->all (),['uid'=>$request->user ()->id]));
+            if ($request->hasFile ('room_cover')){
+                $room->room_cover = Common::upload ('rooms',$request->file ('room_cover'));
+                $room->save();
+            }
+            return Common::apiResponse (true,'created',new RoomResource($room),200);
+        }catch (\Exception $exception){
+            return Common::apiResponse (false,'failed',null,400);
+        }
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show(Request $request,$id)
+    {
+        $request['show'] = true;
+        $room = Room::find($id);
+        if (!$room){
+            return Common::apiResponse(0,'not found',null,404);
+        }
+        return Common::apiResponse (true,'',new RoomResource($room),200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(EditRoomRequest $request, $id)
+    {
+
+
+
+        try {
+            $room = $this->repo->find ($id);
+            if(!$room){
+                return Common::apiResponse (false,'not found',null,404);
+            }
+            if ($room->uid != $request->user ()->id && !in_array ($request->user ()->id,explode (',',$room->room_admin))){
+                return Common::apiResponse (false,'not allowed',null,403);
+            }
+            if ($request->room_name){
+                $room->room_name = $request->room_name;
+            }
+
+            if ($request->hasFile ('room_cover')){
+                $room->room_cover = Common::upload ('rooms',$request->file ('room_cover'));
+            }
+
+            if ($request->free_mic){
+                $room->free_mic = $request->free_mic;
+            }
+
+            if ($request->room_intro){
+                $room->room_intro = $request->room_intro;
+            }
+
+            if ($request->room_pass){
+                $room->room_pass = $request->room_pass;
+            }
+
+            if ($request->room_type){
+                if (!RoomCategory::query ()->where ('id',$request->room_type)->where ('enable',1)->exists ()) return Common::apiResponse (0,'type not found',null,404);
+                $room->room_type = $request->room_type;
+            }
+
+            if ($request->room_class){
+                if (!RoomCategory::query ()->where ('id',$request->room_class)->where ('enable',1)->exists ()) return Common::apiResponse (0,'class not found',null,404);
+                $room->room_type = $request->room_type;
+            }
+            $background_me = '';
+            if ($request->room_background){
+                /*if (!Background::query ()->where ('id',$request->room_background)->where ('enable',1)->exists ()){
+                    return Common::apiResponse (0,'background not found',null,404);
+                }*/
+                if($request->change == 'app'){
+                    $room->room_background = $request->room_background;
+                    RequestBackgroundImage::query()->where('owner_room_id',$room->uid)->where('status',1)->update(['status' => 3]);
+                }
+                if($request->change == 'me'){
+                    RequestBackgroundImage::query()->where('owner_room_id',$room->uid)->where('id','!=',$request->room_background)->where('status',1)->update(['status' => 3]);
+                    $background_update = RequestBackgroundImage::where('id',$request->room_background)->first();
+                    $background_update->status = 1;
+                    $background_update->save();
+                    $background_me = $background_update->img;
+                    $room->room_background = null;
+                }
+
+            }
+
+//            $this->repo->save ($room);
+            $room->save ();
+            $request['owner_id'] = $room->uid;
+
+            $data = [
+                "messageContent"=>[
+                    "message"=>"changeBackground",
+                    "imgbackground"=>$room->room_background?:$background_me,
+                    "roomIntro"=>$room->room_intro?:"",
+                    "roomImg"=>$room->room_cover?:"",
+                    "room_type"=>@$room->myType->name?:"",
+                    "room_name"=>@$room->room_name?:""
+                ]
+            ];
+            $json = json_encode ($data);
+            Common::sendToZego ('SendCustomCommand',$room->id,$request->user ()->id,$json);
+            $request->is_update = true;
+            return $this->enter_room2 ($request);
+
+        }catch (\Exception $exception){
+            return Common::apiResponse (false,'failed',null,400);
+        }
+    }
+
+}
--- app/Http/Controllers/Api/V1/RoomController.php
+++ app/Http/Controllers/Api/V1/RoomController.php
@@ -5,18 +5,13 @@ namespace App\Http\Controllers\Api\V1;
 use App\Helpers\CacheHelper;
 use App\Helpers\Common;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\CreateRoomRequest;
-use App\Http\Requests\EditRoomRequest;
 use App\Http\Resources\Api\V1\BoxUseResource;
 use App\Http\Resources\Api\V2\EnterRoomCollection;
-use App\Http\Resources\Api\V1\FamilyResource;
 use App\Http\Resources\Api\V1\MiniUserResource;
 use App\Http\Resources\Api\V1\PkResource;
 use App\Http\Resources\Api\V1\RoomResource;
 use App\Http\Resources\Api\V1\UserResource;
 use App\Jobs\EnterRoomZigoRequest;
-use App\Models\Agency;
-use App\Models\AgencySallary;
 use App\Models\Background;
 use App\Models\BoxUse;
 use App\Models\EnteredRoom;
@@ -26,235 +21,14 @@ use App\Models\LiveTime;
 use App\Models\Pk;
 use App\Models\Room;
 use App\Models\RoomCategory;
-use App\Models\RoomView;
 use App\Models\User;
 use App\Models\RequestBackgroundImage;
-use App\Models\UserSallary;
-use App\Repositories\Room\RoomRepo;
-use App\Repositories\Room\RoomRepoInterface;
-use App\Traits\HelperTraits\RoomTrait;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 
 class RoomController extends Controller
 {
-
-
-    protected $repo;
-
-    public function __construct (RoomRepoInterface $repo)
-    {
-        $this->repo = $repo;
-    }
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @param Request $request
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function index(Request $request)
-    {
-        $result = $this->repo->all ($request);
-        $user = $request->user ();
-        foreach (Agency::all () as $agency){
-            AgencySallary::query ()->updateOrCreate (
-                [
-                    'agency_id'=>$agency->id,
-                    'month'=>Carbon::now ()->month,
-                    'year'=>Carbon::now ()->year
-                ],
-                [
-                    'agency_id'=>$agency->id,
-                    'month'=>Carbon::now ()->month,
-                    'year'=>Carbon::now ()->year,
-                    'sallary'=>UserSallary::query ()
-                        ->where ('month',Carbon::now ()->month)
-                        ->where ('year',Carbon::now ()->year)
-                        ->where ('user_agency_id',$agency->id)
-                        ->sum ('agency_sallary')
-                ]
-            );
-            $agency->salary;
-        }
-
-        return Common::apiResponse (true,'',RoomResource::collection ($result),200,Common::getPaginates ($result));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function store(Request $request)
-    {
-        $request['show']=true;
-        $request['numid']=rand (111111,999999);
-        try {
-            $room = Room::query ()->where('uid',$request->user ()->id)->first ();
-            if ($room){
-                return Common::apiResponse (true,'you are already have a room',new RoomResource($room),200);
-            }
-           $room = $this->repo->create (array_merge($request->all (),['uid'=>$request->user ()->id]));
-            if ($request->hasFile ('room_cover')){
-                $room->room_cover = Common::upload ('rooms',$request->file ('room_cover'));
-                $room->save();
-            }
-            return Common::apiResponse (true,'created',new RoomResource($room),200);
-        }catch (\Exception $exception){
-            return Common::apiResponse (false,'failed',null,400);
-        }
-
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function show(Request $request,$id)
-    {
-        $request['show'] = true;
-        $room = Room::find($id);
-        if (!$room){
-           return Common::apiResponse(0,'not found',null,404);
-        }
-        return Common::apiResponse (true,'',new RoomResource($room),200);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function update(EditRoomRequest $request, $id)
-    {
-
-
-
-        try {
-            $room = $this->repo->find ($id);
-            if(!$room){
-                return Common::apiResponse (false,'not found',null,404);
-            }
-            if ($room->uid != $request->user ()->id && !in_array ($request->user ()->id,explode (',',$room->room_admin))){
-                return Common::apiResponse (false,'not allowed',null,403);
-            }
-            if ($request->room_name){
-                $room->room_name = $request->room_name;
-            }
-
-            if ($request->hasFile ('room_cover')){
-                $room->room_cover = Common::upload ('rooms',$request->file ('room_cover'));
-            }
-
-            if ($request->free_mic){
-                $room->free_mic = $request->free_mic;
-            }
-
-            if ($request->room_intro){
-                $room->room_intro = $request->room_intro;
-            }
-
-            if ($request->room_pass){
-                $room->room_pass = $request->room_pass;
-            }
-
-            if ($request->room_type){
-                if (!RoomCategory::query ()->where ('id',$request->room_type)->where ('enable',1)->exists ()) return Common::apiResponse (0,'type not found',null,404);
-                $room->room_type = $request->room_type;
-            }
-
-            if ($request->room_class){
-                if (!RoomCategory::query ()->where ('id',$request->room_class)->where ('enable',1)->exists ()) return Common::apiResponse (0,'class not found',null,404);
-                $room->room_type = $request->room_type;
-            }
-            $background_me = '';
-            if ($request->room_background){
-                /*if (!Background::query ()->where ('id',$request->room_background)->where ('enable',1)->exists ()){
-                    return Common::apiResponse (0,'background not found',null,404);
-                }*/
-                if($request->change == 'app'){
-                    $room->room_background = $request->room_background;
-                    RequestBackgroundImage::query()->where('owner_room_id',$room->uid)->where('status',1)->update(['status' => 3]);
-                }
-                if($request->change == 'me'){
-                    RequestBackgroundImage::query()->where('owner_room_id',$room->uid)->where('id','!=',$request->room_background)->where('status',1)->update(['status' => 3]);
-                    $background_update = RequestBackgroundImage::where('id',$request->room_background)->first();
-                    $background_update->status = 1;
-                    $background_update->save();
-                    $background_me = $background_update->img;
-                    $room->room_background = null;
-                }
-
-            }
-
-//            $this->repo->save ($room);
-            $room->save ();
-            $request['owner_id'] = $room->uid;
-
-            $data = [
-                "messageContent"=>[
-                    "message"=>"changeBackground",
-                    "imgbackground"=>$room->room_background?:$background_me,
-                    "roomIntro"=>$room->room_intro?:"",
-                    "roomImg"=>$room->room_cover?:"",
-                    "room_type"=>@$room->myType->name?:"",
-                    "room_name"=>@$room->room_name?:""
-                ]
-            ];
-            $json = json_encode ($data);
-            Common::sendToZego ('SendCustomCommand',$room->id,$request->user ()->id,$json);
-            $request->is_update = true;
-            return $this->enter_room2 ($request);
-
-        }catch (\Exception $exception){
-            return Common::apiResponse (false,'failed',null,400);
-        }
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
-
-
-//------------------------------------------------------------------ops----------------------------------------------------------------
-
 
     public function getAdmins(Request $request){
         if (!$request->owner_id) return Common::apiResponse (0,'missing params',null,422);
@@ -265,9 +39,6 @@ class RoomController extends Controller
         $ads = UserResource::collection ($admins);
         return Common::apiResponse (1,'',$ads,200);
     }
-
-
-
 
     //get_room_by_owner_id
 
