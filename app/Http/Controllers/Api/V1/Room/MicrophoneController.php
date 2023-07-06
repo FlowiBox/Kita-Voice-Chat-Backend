711ade7703b27ca401b96dd53ab7f48905e837c4
Ahmed Ramadan <56490896+AhmedRamadan30@users.noreply.github.com>
2023-07-06 11:39:40 +0300

    Update MicrophoneController.php

--- app/Http/Controllers/Api/V1/Room/MicrophoneController.php
+++ app/Http/Controllers/Api/V1/Room/MicrophoneController.php
@@ -484,6 +484,7 @@ class MicrophoneController extends Controller
             $timer->hours = $hours;
 
             $user_day = UserDay::where('user_id', $uid)->whereDate('created_at', today())->first();
+            dd($user_day, LiveTime::where ('uid',$uid)->whereDate('created_at',today ())->sum('hours'));
             if (LiveTime::where ('uid',$uid)->whereDate('created_at',today ())->sum('hours') >= 0.1 && $user_day->day == 0) {
                 $user_day->day = 1;
                 $user_day->save();
