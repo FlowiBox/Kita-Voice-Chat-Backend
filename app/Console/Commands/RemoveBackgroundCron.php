337c1469b469a9aea1df3b21771cecdb0e2c5e65
‪Ahmed Shhata‬‏ <93527078+leadarbut2022@users.noreply.github.com>
2023-07-10 13:14:42 +0300

    any
    any

--- app/Console/Commands/RemoveBackgroundCron.php
+++ app/Console/Commands/RemoveBackgroundCron.php
@@ -43,19 +43,23 @@ class RemoveBackgroundCron extends Command
     public function handle()
     {
         
-        //\Log::info("Testing Cron is Running ... !");
-        $bgfirst = Background::first();
-        $RequestBackgroundImage = RequestBackgroundImage::whereIn('status',[1,3])->where('created_at', '<', now()->subDays(30)->endOfDay())->get();
-        $owner_ids =  $RequestBackgroundImage->pluck('owner_room_id');
-        $rooms = Room::whereIn('uid',$owner_ids)->update([
-            'room_background' => $bgfirst ? $bgfirst->id : null
-        ]);
-        foreach($RequestBackgroundImage as $img){
-            $path = $img->img;
-            Storage::delete($path);
-            $img->delete();
-        }
-        
+        // \Log::info("Testing Cron is Running ... !");
+
+                        $bgfirst = Background::first();
+                        $RequestBackgroundImage = RequestBackgroundImage::whereIn('status',[1,3])->where('created_at', '<', now()->subDays(30)->endOfDay())->get();
+                        $owner_ids =  $RequestBackgroundImage->pluck('owner_room_id');
+                        $rooms = Room::whereIn('uid',$owner_ids)->update([
+                            'room_background' => $bgfirst ? $bgfirst->id : null
+                        ]);
+                        foreach($RequestBackgroundImage as $img){
+                            $path = $img->img;
+                            Storage::delete($path);
+                            $img->delete();
+                        }
+
+
+                    
+                        
         //$this->info('update-room-user-now:cron Command Run Successfully !');
     }
 }
